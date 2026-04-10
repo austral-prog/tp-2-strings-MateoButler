b60360582d93c32adee7d03c3e2ab298b918d506
MateoButler <mateobutler06@gmail.com>
2026-04-10 10:41:45 -0300

    Delete tp-2-strings-MateoButler-main directory

--- tp-2-strings-MateoButler-main/slice_simple.py
+++
@@ -1,9 +0,0 @@
-def slice_simple():
-    """Dado el texto 'Awesome', imprime distintos substrings
-    usando slicing y lower().
-    """
-    texto = "Awesome"
-
-    print(texto[:3].lower())
-    print(texto[2:5].lower())
-    print(texto.lower())
